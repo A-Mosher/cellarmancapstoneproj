e0ef23aa29a4c1b888daebef3ee7b88fcdfb3f26
A-Mosher <adam.l.mosher@gmail.com>
2021-06-14 19:22:29 -0400

    added user schema into user.js file in models folder. added route for new user in users.js file in routes folder. updated index.js file to add new user model.

---
+++ routes/users.js
@@ -0,0 +1,26 @@
+const { User } = require('../models/user');
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    try {
+        const { error } = validateUser(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+
+        let user = await User.findOne({ email: req.body.email });
+        if (user) return res.status(400).send('User already registered.');
+
+        user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        await user.save();
+        return res.send({ _id, name: user.name, email: user.email });
+    }   catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
+module.exports = router;
