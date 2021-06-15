ac94dc381b6fb2f4a43c5f539346859e4078b787
A-Mosher <adam.l.mosher@gmail.com>
2021-06-14 20:40:29 -0400

    fixed some mistakes with the user model and route and installed bycrypt to add hashing to passwords for user login. also added auth.js for login validation

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const Joi = require('joi');
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true, minlength: 2, maxlength: 50 },
     email: { type: String, unique: true, required: true, minlength: 5, maxlength: 255 },
-    password: { type: String, required: true, maxlength: 1024, minlength: 5 },
+    password: { type: String, required: true, minlength: 5, maxlength: 1024 },
 });
 
 const User = mongoose.model('User', userSchema);
@@ -14,10 +14,10 @@ function validateUser(user) {
     const schema = Joi.object({
         name: Joi.string().min(2).max(50).required(),
         email: Joi.string().min(5).max(255).required().email(),
-        password: Joi.string().min(2).max(1024).required(),
+        password: Joi.string().min(5).max(1024).required(),
     });
     return schema.validate(user);
 }
 
-exports.User = user;
+exports.User = User;
 exports.validateUser = validateUser;
